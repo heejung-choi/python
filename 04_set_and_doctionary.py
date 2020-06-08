35eceb3713bfafcc2ad5a01d9ea74f8829de821a
heejung-choi <i9i91212@naver.com>
2020-06-08 14:20:42 +0900

    dictionary 예제

--- 04_set_and_doctionary.py
+++
@@ -1,9 +0,0 @@
-# 1. set 대중적으로 많이 쓰지는 않는다.
-set_a = {1,2,3}
-set_b = {3,6,9}
-
-print(set_a - set_b) #차집합
-print(set_a | set_b) #합집합
-print(set_a & set_b) #교집합
-
-# set_c= {1,1,1} => 1 중복을 제거하기 때문에 어차피 1이다.
